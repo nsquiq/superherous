f0efd65118b3bcd3655019a911a55accb7aa7e28
Nuray <210103331@stu.sdu.edu.kz>
2023-11-09 23:50:43 +0600

    inf

--- app/src/main/java/com/example/superherous/MainActivity.kt
+++ app/src/main/java/com/example/superherous/MainActivity.kt
@@ -1,12 +1,18 @@
-package com.example.superherous
+import androidx.annotation.StringRes
+import com.example.superherous.R
+
+tackage com.example.superherous
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.superherous.data.heros
 import com.example.superherous.data.Hero
@@ -58,3 +65,23 @@ fun HeroItem(
         HeroInformation(hero.name, hero.description)
     }
 }
+
+@Composable
+fun HeroIcon(
+    @DrawableRes heroIcon: Int,
+    modifier: Modifier = Modifier
+){
+    Image(
+        modifier = modifier
+            .size(dimensionResource(R.dimen.image_size))
+            .padding(dimensionResource(R.dimen.padding_small)),
+        painter = painterResource(heroIcon),
+        contentDescription = null
+    )
+}
+
+@Composable
+fun HeroInformation(
+    @StringRes heroName: Int,
+    modifier: Modifier = Modifier
+){ }
