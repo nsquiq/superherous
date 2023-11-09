dce313f5361b53defbde2baf13dd00b5d864cbc9
Nuray <210103331@stu.sdu.edu.kz>
2023-11-09 23:41:37 +0600

    icon inf

--- app/src/main/java/com/example/superherous/MainActivity.kt
+++ app/src/main/java/com/example/superherous/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.superherous
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.superherous.data.heros
 import com.example.superherous.data.Hero
@@ -44,6 +48,13 @@ fun SuperApp(){
 fun HeroItem(
     hero:Hero,
     modifier: Modifier = Modifier
-){
-
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(dimensionResource(R.dimen.padding_small))
+    ) {
+        HeroIcon(hero.imageResourceId)
+        HeroInformation(hero.name, hero.description)
+    }
 }
