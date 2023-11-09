cd65073c520d3ff533f8b8a9cfc127b506e3b6bd
Nuray <210103331@stu.sdu.edu.kz>
2023-11-09 23:32:50 +0600

    item

--- app/src/main/java/com/example/superherous/MainActivity.kt
+++ app/src/main/java/com/example/superherous/MainActivity.kt
@@ -4,12 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.superherous.data.heros
+import com.example.superherous.data.Hero
 import com.example.superherous.ui.theme.SuperherousTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +25,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SuperApp()
                 }
             }
         }
@@ -30,17 +33,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SuperherousTheme {
-        Greeting("Android")
+fun SuperApp(){
+    LazyColumn{
+        items(heros){
+            HeroItem(hero = it)
+        }
     }
+}
+@Composable
+fun HeroItem(
+    hero:Hero,
+    modifier: Modifier = Modifier
+){
+
 }
